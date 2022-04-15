1cbc2f451d381b41c583d4b8d4ec3a29930f0195
Nisha Rathod <nisha.rathod@zymr.com>
2022-04-15 22:38:33 +0530

    Implemented D2H Commnad line application

---
+++ src/services/accounts.js
@@ -0,0 +1,42 @@
+const conf = new (require("conf"))();
+const chalk = require("chalk")
+const Tariff = require("./tariff");
+
+class Account extends Tariff{
+  constructor() {
+    super();
+    conf.set("balance", 0);
+  }
+
+  /**
+   * @account - getBalance()
+   * @description - This method allows you to view your balance in the account
+   * @param -
+   * @returns - {string} balance.
+   */
+  getBalance() {
+    return conf.get("balance");
+  }
+
+  /**
+   * @account - addCash()
+   * @description - This method allows you to add cash to the account
+   * @param - {Integer} cash
+   * @returns - {string} message.
+   */
+  addCash(cash) {
+    let amount = conf.get("balance");
+
+    if (cash > 0) {
+      amount = amount + parseInt(cash);
+      conf.set("balance", amount);
+      return chalk.green.bold(
+        `${cash} amount added successfully, your balance is ${amount}.`
+      );
+    } else {
+      return chalk.red.bold("Please enter a valid amount.");
+    }
+  }
+}
+
+module.exports = Account;
